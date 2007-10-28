a670436d4bae6a7d3c25542ae3ec69fce52c9dc7
zaxl <zaxl@1ee56e69-fe3a-0410-901e-914b4450df7a>
2007-10-28 16:18:15 +0000

    Teach garxor to process directories and to check file headers
    git-svn-id: svn://svn.code.sf.net/p/libgarmin/svn@70 1ee56e69-fe3a-0410-901e-914b4450df7a

--- libgarmin/dev/utils/garxor.c
+++ libgarmin/dev/utils/garxor.c
@@ -1,45 +1,126 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <malloc.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include "../src/GarminTypedef.h"
 
-
-int main(int argc, char **argv)
+static int gar_xor_file(char *in, char *out)
 {
 	char buf[4096];
-	int i;
 	int fd, fd1;
-	int rc;
 	int first = 1;
 	char xorb = 0;
-	if (argc != 3) {
-		fprintf(stderr, "usage: %s infile outfile\n", argv[0]);
-		return -1;
-	}
-	fd = open(argv[1], O_RDONLY);
+	int ret = -1;
+	int rc,i;
+	fd = open(in, O_RDONLY);
 	if (fd < 0) {
-		fprintf(stderr, "Can not open:%s\n", argv[1]);
+		fprintf(stderr, "Can not open:%s\n", in);
 		return -1;
 	}
-	fd1 = open(argv[2], O_RDWR|O_CREAT|O_TRUNC, 0660);
+	fd1 = open(out, O_RDWR|O_CREAT|O_TRUNC, 0660);
 	if (fd1 < 0) {
-		fprintf(stderr, "Can not open:%s\n", argv[2]);
+		fprintf(stderr, "Can not open:%s\n", out);
 		return -1;
 	}
 	while ((rc = read(fd, buf, sizeof(buf))) > 0) {
 		i = 0;
 		if (first) {
 			xorb = buf[0];
-			first = 0;
-			// i ++;
 		}
 		for (;i<rc; i++) {
 			buf[i] ^= xorb;
 		}
+		if (first) {
+			struct hdr_img_t *img = (struct hdr_img_t *)buf;
+			if (strncmp(img->signature,"DSKIMG",6)) {
+				fprintf(stderr, "%s is not garmin image - invalid signature\n",
+					in);
+				goto out;
+			}
+			if (strncmp(img->identifier,"GARMIN",6)) {
+				fprintf(stderr, "%s is not garmin image - invalid identifier\n",
+					in);
+				goto out;
+			}
+			first = 0;
+		}
 		write(fd1, buf, rc);
 	}
+	ret = 0;
+out:
 	close(fd);
 	close(fd1);
+	if (ret < 0)
+		unlink(out);
+	return ret;
+}
+
+static int gar_xor_dir(char *path)
+{
+	struct dirent **namelist;
+	int n, rc;
+	int cnt = 0;
+	char in[4096];
+	char out[4096];
+	char ren[4096];
+	n = scandir(path, &namelist, 0, NULL);
+	if (n < 0) {
+		fprintf(stderr, "Bad directory path\n");
+		return -1;
+	}
+	while(n--) {
+		if ( *namelist[n]->d_name == '.') {
+			free(namelist[n]);
+			continue;
+		}
+		fprintf(stderr, "Processing: [%s]\n", namelist[n]->d_name);
+		sprintf(in,"%s/%s", path, namelist[n]->d_name);
+		sprintf(out,"%s/%s.tmp", path, namelist[n]->d_name);
+		rc = gar_xor_file(in, out);
+		if (rc == 0) {
+			sprintf(ren,"%s/%s.orig", path, namelist[n]->d_name);
+			if (!rename(in, ren)) {
+				if (!rename(out, in)) {
+					fprintf(stdout, "Converted [%s]\n",
+							namelist[n]->d_name);
+					unlink(ren);
+					cnt++;
+				} else {
+					fprintf(stderr, "Error renaming [%s] to [%s]\n",
+						out, in);
+					// Can't help if that fails
+					rename(ren, in);
+				}
+			} else {
+				fprintf(stderr, "Error renaming [%s] to [%s]\n",
+					in, ren);
+				unlink(out);
+			}
+			cnt++;
+		} else {
+			fprintf(stderr, "Error processing: [%s]\n", 
+					namelist[n]->d_name);
+		}
+		free(namelist[n]);
+	}
+	free(namelist);
+	return cnt;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s infile outfile\n", argv[0]);
+		return -1;
+	}
+	if (!strcmp(argv[1], "-d")) {
+		gar_xor_dir(argv[2]);
+	} else {
+		gar_xor_file(argv[1], argv[2]);
+	}
 	return 0;
 }
